ee2c323c2492ccaeb93ca095c1860cc833a5955b
Nidhi A Lalwani <157901439+nidhilalwani01@users.noreply.github.com>
2026-02-18 00:17:19 +0000

    add initial HTML and JavaScript files with basic console and alert messages

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hello, World!");
+console.error("This is my first JavaScript error!");
+alert("Nidhi Lalwani");
+alert("Dong Kim");
+alert("Joshua Garnett");
+alert("Jacob Roginski");
+alert("Jonathan Luna");
+console.log(console);
